ca1f1b58391ca52480c7f56b7ad8bfcdbfbb2e17
Aditya <102513627+Adityapng@users.noreply.github.com>
2024-09-10 22:40:04 +0530

    changes in route js and database js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -9,8 +9,8 @@ import { connectToDB } from "@utils/database";
 const handler = NextAuth({
   providers: [
     GoogleProvider({
-      clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
-      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
 
--- utils/database.js
+++ utils/database.js
@@ -9,7 +9,7 @@ export const connectToDB = async () => {
   }
 
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "User",
       useNewUrlParser: true,
       useUnifiedTopology: true,
