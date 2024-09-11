442448c6b85cac3516c906e64f44e80805870d26
Aditya <102513627+Adityapng@users.noreply.github.com>
2024-09-11 13:16:02 +0530

    changes in database js 2

--- utils/database.js
+++ utils/database.js
@@ -6,18 +6,24 @@ export const connectToDB = async () => {
 
   if (isConnected) {
     console.log("MongoDB is already connected");
+    return;
+  }
+
+  if (mongoose.connection.readyState === 1) {
+    console.log("MongoDB is already connected (via mongoose.connection).");
+    return;
   }
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: "User",
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      maxPoolSize: 10, // Adjust as needed
     });
 
     isConnected = true;
-    console.log("mongoDB connected");
+    console.log("MongoDB connected successfully.");
   } catch (error) {
-    console.log(error);
+    console.error("Error connecting to MongoDB:", error);
+    throw new Error("MongoDB connection failed");
   }
 };
